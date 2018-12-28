5d651078a85e44266b098fcaefb209e7e4f1872c
april <martinezapril56@yahoo.com>
2018-12-27 19:04:13 -0500

    edit

--- notes/src/EditNote/EditNote.js
+++ notes/src/EditNote/EditNote.js
@@ -22,13 +22,44 @@ class EditNote extends Component
     componentWillMount()
     {
         let routeId = this.props.match.parm.id;
-        let matched = this.props.notesArray.filter( ( item._id === routeId ) );
+        let matched = this.props.notesArray.filter( ( item)=> item._id === routeId ) ;
         this.setState({matched})
     }
 
     handleUpdate = () =>
     {
         this.props.editNote( this.state.matched[ 0 ] )
-        this.props.history.push
+        this.props.history.push('/');
+    }
+    render(){
+        return (
+            <div className='noteView_container'>
+                <div className='noteView_topContent'>
+                    <h3 className='content_header'>
+                        Edit Note:
+                    </h3>
+                </div>
+                <div className='createNote_form'>
+                    <input
+                        type='text'
+                        className='createNote_title'
+                        name='title'
+                        value={this.state.matched[ 0 ].title}
+                        onChange={this.handleChange}
+                    />
+                    <textarea
+                        className='createNote_body'
+                        value={this.state.matched[ 0 ].body}
+                        rows='20'
+                        name='body'
+                        onChange={this.handlehange} />
+                    <div
+                        classNAme='nav_button createNote_button'
+                        onClick={this.handleUpdate}
+                    >Update</div>
+                </div>
+            </div>
+        );
     }
 }
+export default connect( mapStateToProps, { editNote } )( EditNote );
